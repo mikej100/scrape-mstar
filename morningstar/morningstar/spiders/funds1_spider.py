59b2ec7c49da89103e3730028f4613e59e1fc0df
Mike Jennings <mike.jennings@chwd.uk>
2021-07-16 00:59:24 +0100

    Read funds Morningstar symbols from investments book and fetch prices.
    TODO similar for equities.

--- morningstar/morningstar/spiders/funds1_spider.py
+++ morningstar/morningstar/spiders/funds1_spider.py
@@ -1,25 +1,60 @@
 import scrapy
 import re
-
+import pandas as pd
+from os.path import exists
 
 class Funds1Spider(scrapy.Spider):
     name = "funds1"
 
     def start_requests(self):
-        fundIds = [
-            "F0000103JP",
-            "F0GBR04SAT"
-        ]
-        urlStem = 'https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id='
+        # fund_symbols = [
+        #     "0P0001HPYK"
+        # ]
+        symbols =  self.get_ms_symbols()
 
-        for fundId in fundIds:
-            yield scrapy.Request(url=urlStem + fundId, callback=self.parse)
+        url_stem = 'https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id='
+
+        for fund_symbol in symbols['fund_symbols']:
+            yield scrapy.Request(url=url_stem + fund_symbol, callback=self.parse)
 
     def parse(self, response):
-        isinText = response.xpath("//td[contains(.,'ISIN')]/following-sibling::*/text()").getall()[1]
-        priceText =  response.xpath("//td[contains(.,'NAV')]/following-sibling::*/text()").getall()[1]
+        isin_text = response.xpath("//td[text()='ISIN']/following-sibling::*/text()").getall()[1]
+
+        price_elems =  response.xpath("//td[text()='NAV']/following-sibling::*/text()").getall()
+        if len(price_elems) > 1 :
+            price_text = price_elems[1]
+        else:
+            # Some funds have Closing Price rather than NAV
+            price_text =  response.xpath("//td[text()='Closing Price']/following-sibling::*/text()").getall()[1]
+
         yield {
-            "isin" : isinText,
-            "currency" : re.search("^.{3}", priceText).group(),
-            "price": re.search(r"[0-9]+\.?[0-9]+",priceText).group()
+            "isin" : isin_text,
+            "currency" : re.search("^.{3}", price_text).group(),
+            "price": re.search(r"[0-9]+\.?[0-9]+",price_text).group()
         }
+
+    def get_ms_symbols(self):
+        inv_book_name = "../../../20210622 Investments book.xlsx"
+        funds_sheet_name = "FundsBase"
+
+        df =  pd.read_excel(inv_book_name, sheet_name = funds_sheet_name)
+
+
+        funds = df\
+            .pipe( lambda df_: df_[df_['Type'] == "Fund"])
+        # funds = df[df['Type'] =='Fund']
+        funds2 = funds[funds['Morningstar symbol'].apply(lambda s: isinstance(s, str))]
+        fund_symbols = funds2['Morningstar symbol']
+
+        equities = df\
+            .pipe( lambda df_: df_[df_['Type'] == "Equity"])
+        # funds = df[df['Type'] =='Fund']
+        equities2 = equities[equities['Morningstar symbol'].apply(lambda s: isinstance(s, str))]
+        equity_symbols = equities2['Morningstar symbol']
+        symbols = {
+            "fund_symbols": fund_symbols,
+            "equity_symbols": equity_symbols
+        }
+        return symbols
+
+        
