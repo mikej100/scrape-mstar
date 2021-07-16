7396fc0b70f44ad356b7cdc77752ea9f72076200
Mike Jennings <mike.jennings@chwd.uk>
2021-07-16 02:32:23 +0100

    Retrieve prices for equities. Disable robot obedience.

--- morningstar/morningstar/spiders/funds1_spider.py
+++ morningstar/morningstar/spiders/funds1_spider.py
@@ -12,12 +12,17 @@ class Funds1Spider(scrapy.Spider):
         # ]
         symbols =  self.get_ms_symbols()
 
-        url_stem = 'https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id='
+        url_stem_funds = 'https://www.morningstar.co.uk/uk/funds/snapshot/snapshot.aspx?id='
+        url_stem_equities = 'https://tools.morningstar.co.uk/uk/stockreport/default.aspx?Site=uk&id='
 
-        for fund_symbol in symbols['fund_symbols']:
-            yield scrapy.Request(url=url_stem + fund_symbol, callback=self.parse)
+        for symbol in symbols['fund_symbols']:
+            yield scrapy.Request(url=url_stem_funds + symbol, callback=self.parse_funds)
 
-    def parse(self, response):
+        for symbol in symbols['equity_symbols']:
+            yield scrapy.Request(url=url_stem_equities + symbol, callback=self.parse_equities)
+
+
+    def parse_funds(self, response):
         isin_text = response.xpath("//td[text()='ISIN']/following-sibling::*/text()").getall()[1]
 
         price_elems =  response.xpath("//td[text()='NAV']/following-sibling::*/text()").getall()
@@ -31,6 +36,16 @@ class Funds1Spider(scrapy.Spider):
             "isin" : isin_text,
             "currency" : re.search("^.{3}", price_text).group(),
             "price": re.search(r"[0-9]+\.?[0-9]+",price_text).group()
+        }
+
+    def parse_equities(self, response):
+        isin_text =	response.xpath("//td[@id='Col0Isin']/text()").getall()[0]
+        price_text = response.xpath("//span[@id='Col0Price']/text()").getall()[0]
+        currency_text = response.xpath("//p[@id='Col0PriceTime']/text()").getall()[2]
+        yield {
+            "isin" : isin_text,
+            "currency" : re.search(r"\|\s(\w{3})", currency_text).group(1),
+            "price": price_text
         }
 
     def get_ms_symbols(self):
